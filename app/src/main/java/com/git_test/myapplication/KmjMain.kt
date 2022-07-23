d8382f6d1ec7c7913f0c6e9e188bb2a5c78e9bf6
kmj <nex1223@naver.com>
2022-07-23 15:50:48 +0900

    :lipstick: 버튼 리스너 추가

---
+++ app/src/main/java/com/git_test/myapplication/KmjMain.kt
@@ -0,0 +1,4 @@
+package com.git_test.myapplication
+
+class KmjMain {
+}
--- app/src/main/java/com/git_test/myapplication/MainActivity.kt
+++ app/src/main/java/com/git_test/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.git_test.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.git_test.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root)
+        setButton()
 
+    }
 
+    private fun setButton(){
+        binding.btn.setOnClickListener {
+            Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+        }
     }
 }
